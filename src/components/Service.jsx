c04c9d933eb861561a2ab575d7508ee0590f47c6
Salvador <salvador.on@outlook.com>
2020-07-02 19:24:17 -0300

    style pagination

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faTwitter, faInstagram, faYoutube } from "@fortawesome/free-brands-svg-icons";
 
-const Service = () => {
+const Service = ({service}) => {
   return (
-    <div className="card mx-4" style={{width: "18rem"}}>
-      <img className="card-img-top" style={{height: "18rem"}} src="https://picsum.photos/200/300"/>
+    <div className="mx-4" style={{width: "18rem"}}>
+      <img className="card-img-top rounded" style={{height: "18rem"}} src={service.image_url}/>
       <div className="card-body">
-        <h5 className="card-text text-center">Service</h5>
+        <h5 className="card-text text-center text-capitalize font-weight-bold">{service.name}</h5>
+        <hr className="w-50" style={{borderBottom: "3px #b8c8cf dotted"}}/>
+        <h6 className="text-center text-capitalize font-weight-bold" style={{color: "#8c9091"}}>{service.description}</h6>
+
+        <div className="d-flex justify-content-center">
+        <FontAwesomeIcon icon={faFacebook} style={{color: "#8c9091"}} className={"fa-2x mx-2 p-1 border border-secondary rounded-circle"}/>
+        <FontAwesomeIcon icon={faTwitter} style={{color: "#8c9091"}} className={"fa-2x mx-2 p-1 border border-secondary rounded-circle"}/>
+        <FontAwesomeIcon icon={faInstagram} style={{color: "#8c9091"}} className={"fa-2x mx-2 p-1 border border-secondary rounded-circle"}/>
       </div>
+      </div>
+      
     </div>
     );
 }
