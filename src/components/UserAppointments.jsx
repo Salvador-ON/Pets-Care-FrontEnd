1738a3096a8c8be5dbf093f5fba5fbf65cbe52c7
Salvador <salvador.on@outlook.com>
2020-07-02 13:34:51 -0300

    set up appoinment filter

--- src/components/UserAppointments.jsx
+++ src/components/UserAppointments.jsx
@@ -11,10 +11,6 @@ const UserAppointments = () => {
     useAppointmentPast(value)
   }
 
-  const handleForm = (e) => {
-    SetAppointmenstPast(e.target.checked)
-  }
-
   const SetAppoList = (data) => {
     useAppoList(data);
   };
@@ -34,8 +30,45 @@ const UserAppointments = () => {
   React.useEffect(() => {
     getAppointments();
   }, []);
+
+  const dateToday = () => {
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    const yyyy = today.getFullYear();
+    return yyyy + '-' + mm + '-' + dd;
+  }
+
+  const pastAppointments = () => {
+    return appoList.filter((item) => {
+      let date = new Date(item.date)
+      date.setHours(item.time.split("T")[1].split(":")[0])
+      return (date < new Date())})
+    }
+
+  const futureAppointments = () => {
+    return appoList.filter((item) => {
+      let date = new Date(item.date)
+      date.setHours(item.time.split("T")[1].split(":")[0])
+      return (date > new Date())})
+    }
+      
+  
+
+  const handleForm = (e) => {
+    SetAppointmenstPast(e.target.checked)
+    let date = new Date("2020-06-29")
+    date.setHours(7);
+    console.log(date);
+  
+    console.log(new Date())
+    console.log(new Date("2020-06-29") < new Date())
+    console.log("pt", pastAppointments());
+    console.log("ft", futureAppointments());
+  
+  }
   return (
-    <div className="ServiceList mt-2">
+    <div className="ServiceList mt-2 mr-2">
       
   <h4 className="mb-0"> { !appointmentPast? "Requested Appointments": "Previous Appointments" } </h4>
 
@@ -62,7 +95,8 @@ const UserAppointments = () => {
           </tr>
         </thead>
         <tbody>
-          {appoList.map((appo) => (
+          {/* {(!appointmentPast? futureAppointments() : pastAppointments() )} */}
+          {(!appointmentPast? futureAppointments() : pastAppointments() ).map((appo) => (
             <tr key={appo.id}>
               <td>{appo.id}</td>
               <td>{appo.date}</td>
