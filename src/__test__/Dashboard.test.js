36538787c6a3d742825fb93f9731c90063c4e25d
Salvador <salvador.on@outlook.com>
2020-07-06 13:05:13 -0300

    fix linterns

--- src/__test__/Dashboard.test.js
+++ src/__test__/Dashboard.test.js
@@ -1,44 +1,36 @@
 import React from 'react';
-import { render, fireEvent, cleanup, waitForElement } from './test-utils';
-import '@testing-library/jest-dom'
-import axios from "axios";
+import axios from 'axios';
+import {
+  render, fireEvent, cleanup, waitForElement,
+} from './test-utils';
+import '@testing-library/jest-dom';
 import App from '../containers/App';
+
 afterEach(cleanup);
 
 describe('App', () => {
-
   axios.get.mockResolvedValueOnce({
-    data: { 
-      logged_in: "LOGGED_IN", 
-      user:{
-        name: "Jhon", 
-        role: "admin"}}
-})
+    data: {
+      logged_in: 'LOGGED_IN',
+      user: {
+        name: 'Jhon',
+        role: 'admin',
+      },
+    },
+  });
 
-  test("user is logged and dashboar button shows",  async () => {
-
+  test('user is logged and dashboar button shows', async () => {
     const { container, getByTestId } = render(<App />);
 
-    const dashboard = await waitForElement(() => 
-      getByTestId("HidenDash")
-    )
+    const dashboard = await waitForElement(() => getByTestId('HidenDash'));
 
-    expect(dashboard.innerHTML).toMatch('Dashboard')
+    expect(dashboard.innerHTML).toMatch('Dashboard');
     fireEvent.click(dashboard);
-    expect(container.innerHTML).toMatch("Welcome");
-    expect(container.innerHTML).toMatch("Jhon");
-    expect(container.innerHTML).toMatch("Services");
-    expect(container.innerHTML).toMatch("Add Services");
-    expect(container.innerHTML).toMatch("Appointments");
-    expect(container.innerHTML).toMatch("My Appointments");
-
+    expect(container.innerHTML).toMatch('Welcome');
+    expect(container.innerHTML).toMatch('Jhon');
+    expect(container.innerHTML).toMatch('Services');
+    expect(container.innerHTML).toMatch('Add Services');
+    expect(container.innerHTML).toMatch('Appointments');
+    expect(container.innerHTML).toMatch('My Appointments');
+  });
 });
-
-
-
-
-  
-
-  
-});
- 
--- src/__test__/DashboardServices.test.js
+++ src/__test__/DashboardServices.test.js
@@ -1,44 +1,36 @@
 import React from 'react';
-import { render, fireEvent, cleanup, waitForElement } from './test-utils';
-import '@testing-library/jest-dom'
-import axios from "axios";
+import axios from 'axios';
+import {
+  render, fireEvent, cleanup, waitForElement,
+} from './test-utils';
+import '@testing-library/jest-dom';
 import App from '../containers/App';
 
 afterEach(cleanup);
 
 describe('App', () => {
-
   axios.get.mockResolvedValueOnce({
-    data: { 
-      logged_in: "LOGGED_IN", 
-      user:{
-        name: "Jhon", 
-        role: "admin"}}
-})
+    data: {
+      logged_in: 'LOGGED_IN',
+      user: {
+        name: 'Jhon',
+        role: 'admin',
+      },
+    },
+  });
 
-  test("user is logged and dashboard shows services",  async () => {
-
+  test('user is logged and dashboard shows services', async () => {
     const { container, getByTestId } = render(<App />);
 
-    const dashboard = await waitForElement(() => 
-      getByTestId("HidenDash")
-    )
+    const dashboard = await waitForElement(() => getByTestId('HidenDash'));
 
-    expect(dashboard.innerHTML).toMatch('Dashboard')
+    expect(dashboard.innerHTML).toMatch('Dashboard');
     fireEvent.click(dashboard);
 
-    const act=(() => {
-      fireEvent.click(getByTestId("ServiceAdmin"));
+    const act = (() => {
+      fireEvent.click(getByTestId('ServiceAdmin'));
     });
-    
-    
-    expect(container.innerHTML).toMatch("Services");
+    act();
+    expect(container.innerHTML).toMatch('Services');
+  });
 });
-
-
-
-  
-
-  
-});
- 
