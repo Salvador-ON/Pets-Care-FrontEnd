452174d1fb208e2f6d7715c81105bb4f53d28275
Salvador <salvador.on@outlook.com>
2020-07-08 18:36:58 -0300

    set log in and sign up mobile

--- src/components/auth/AdminRegistration.jsx
+++ src/components/auth/AdminRegistration.jsx
@@ -6,6 +6,7 @@ import "../../styles/Registration.css";
 import { useDispatch } from "react-redux";
 import { LogIn } from "../../actions/index.js";
 import { useHistory } from "react-router-dom";
+import HidenNav from '../HidenNav';
 
 const Registration = () => {
   let history = useHistory();
@@ -93,12 +94,14 @@ const Registration = () => {
 
   return (
     <div>
+      <div className="d-sm-none"><HidenNav/></div>
       <NavBar option={"signup"} />
       <div className="float-right SignupContainer">
         <h1 className="SignupTile text-center">Admin Registration</h1>
 
-        <div className="d-flex">
-          <form onSubmit={HandleSubmit} className="w-50 mx-2">
+        <div className="d-flex flex-column flex-sm-row">
+          <div className="col-12 col-sm-6">
+          <form onSubmit={HandleSubmit} className="mx-4">
             <div className="form-group">
               <label htmlFor="token">Admin or Employe Token</label>
               <input
@@ -188,8 +191,12 @@ const Registration = () => {
               {error.value ? <Error error={error.data} /> : null}
             </div>
           </form>
+          </div>
+          
+          <div className="col-12 col-sm-6">
+           <div className="dogImageContainer mx-auto mx-sm-0"></div> 
+          </div>
 
-          <div className="dogImageContainer"></div>
         </div>
       </div>
     </div>
